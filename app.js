7fecedc995acd52051da7a467c38a4acbc5954b4
kaleboni <83687079+kaleboni@users.noreply.github.com>
2024-04-23 12:19:46 -0600

    completo
    completo curso

--- app.js
+++ app.js
@@ -1,1 +1,23 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Hora del Desafío'
 
+function mostarMensajeEnLaConsola() {
+    console.log('El botón fue clicado!')
+}
+
+function mostrarAlerta() {
+    let ciudad = prompt("Por favor, ingresa el nombre de una ciudad de Brasil:");
+    alert("Estuve en " + ciudad + " y me acordé de ti.");
+}
+
+function mostarAlerta(){
+    alert("AMO JS");
+} 
+
+
+function sumaDosNumeros(){
+    let primerNumero = parseInt(prompt('Digite el primer numero'));
+    let segunNumero = parseInt(prompt('Digite el segundo numero'));
+    let resultado = primerNumero + segunNumero;
+    alert('${primerNumero} + ${segunNumero} = ${resultado}')
+}
